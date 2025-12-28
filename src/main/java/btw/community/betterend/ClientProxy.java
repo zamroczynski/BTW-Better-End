090752d7acf2f5e3b24fcae722aef2874ee2aac9
Damian Zamroczynski <damianzamroczynski@gmail.com>
2025-12-28 10:27:01 +0100

    Fix obfuscation for Ender Mite

--- src/main/java/btw/community/betterend/ClientProxy.java
+++ src/main/java/btw/community/betterend/ClientProxy.java
@@ -2,6 +2,7 @@ package btw.community.betterend;
 
 import btw.community.betterend.client.RenderEnderMite;
 import btw.community.betterend.entity.EntityEnderMite;
+import net.minecraft.src.Render;
 import net.minecraft.src.RenderManager;
 import java.lang.reflect.Field;
 import java.util.Map;
@@ -10,13 +11,21 @@ public class ClientProxy {
     public static void registerRenderers() {
         try {
             RenderEnderMite renderer = new RenderEnderMite();
-
             renderer.setRenderManager(RenderManager.instance);
+            Field renderMapField = null;
+            for (Field field : RenderManager.class.getDeclaredFields()) {
+                if (Map.class.isAssignableFrom(field.getType())) {
+                    renderMapField = field;
+                    break;
+                }
+            }
 
-            Field renderMapField = RenderManager.class.getDeclaredField("entityRenderMap");
+            if (renderMapField == null) {
+                throw new RuntimeException("BetterEnd Error: Could not find entityRenderMap field in RenderManager!");
+            }
+
             renderMapField.setAccessible(true);
-
-            Map<Class, Object> renderMap = (Map<Class, Object>) renderMapField.get(RenderManager.instance);
+            Map<Class, Render> renderMap = (Map<Class, Render>) renderMapField.get(RenderManager.instance);
             renderMap.put(EntityEnderMite.class, renderer);
         } catch (Exception e) {
             System.err.println("BetterEnd Error: Failed to register renderers!");
