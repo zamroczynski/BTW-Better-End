189b3f0e8f7bfa9608eb397203056e47a4bd984c
Damian Zamroczynski <damianzamroczynski@gmail.com>
2025-12-28 10:51:30 +0100

    Change the texture of Mite Gland

--- src/main/java/btw/community/betterend/item/ItemMiteGland.java
+++ src/main/java/btw/community/betterend/item/ItemMiteGland.java
@@ -1,13 +1,22 @@
 package btw.community.betterend.item;
 
+import btw.community.betterend.client.MiteGlandSprite;
 import net.minecraft.src.CreativeTabs;
+import net.minecraft.src.IconRegister;
 import net.minecraft.src.Item;
+import net.minecraft.src.TextureAtlasSprite;
 
 public class ItemMiteGland extends Item {
     public ItemMiteGland(int id) {
         super(id);
         this.setUnlocalizedName("betterend.mite_gland");
         this.setCreativeTab(CreativeTabs.tabMaterials);
-        this.setTextureName("btw:witch_wart");
+    }
+
+    @Override
+    public void registerIcons(IconRegister register) {
+        TextureAtlasSprite customSprite = new MiteGlandSprite("betterend:mite_gland", "btw:witch_wart");
+
+        this.itemIcon = register.registerIcon("betterend:mite_gland", customSprite);
     }
 }
